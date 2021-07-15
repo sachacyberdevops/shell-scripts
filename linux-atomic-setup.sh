5044e682ecd82595de2879aed740a508f160711d
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-07-15 11:51:31 +1000

    Update linux-atomic-setup.sh

--- linux-atomic-setup.sh
+++ linux-atomic-setup.sh
@@ -4,6 +4,19 @@
 sudo apt-get update -y;
 # Install pre-requisite packages.
 sudo apt-get install -y wget apt-transport-https software-properties-common;
+# Install Audit Daemon
+sudo apt-get install auditd audispd-plugins -y;
+# Add audit filter rules to capture successful process executions
+sudo auditctl -a always,exit -F arch=b32 -S execve,execveat;
+sudo auditctl -a always,exit -F arch=b64 -S execve,execveat;
+sudo auditctl -w /bin/kmod -p x -k kernelmodules;
+sudo auditctl -w /var/log/audit -p wxa -k audittampering;
+sudo auditctl -w /etc/audit -p wxa -k audittampering;
+sudo auditctl -w /etc/passwd -p wxa -k usergroup;
+sudo auditctl -w /etc/group -p wxa -k usergroup;
+sudo auditctl -w /etc/pam.d -p wxa -k pam;
+# Command to confirm list of added audit filters
+sudo auditctl -l;
 # Download the Microsoft repository GPG keys
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb;
 # Register the Microsoft repository GPG keys
