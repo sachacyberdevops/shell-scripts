35af9f180d8a2ffcac0ca41b8b334597138e2fba
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-07-15 10:15:40 +1000

    Update atomic-red-install-linux.ps1

--- atomic-red-install-linux.ps1
+++ atomic-red-install-linux.ps1
@@ -1,1 +1,3 @@
-
+IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing);
+Install-AtomicRedTeam -getAtomics;
+Invoke-AtomicTest
