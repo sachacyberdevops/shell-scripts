1ae5b7c037ad383b12601dbad0ce9a4359c086b4
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-07-15 22:02:22 +1000

    Update linux-auditd.sh

--- linux-auditd.sh
+++ linux-auditd.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Going into Root
 sudo -i;
+# Installing auditd
 sudo apt install -y auditd;
-sudo curl https://raw.githubusercontent.com/DFW1N/ansible-tower/main/credentials -o mitre_attack.rules;
-touch /etc/audit/rules.d/mitre_attack.rules;
+# Pulling mitre_attack.rules from a Github Repo
+sudo curl https://raw.githubusercontent.com/DFW1N/shell-scripts/main/audit-attack.rules -o /etc/audit/rules.d/mitre_attack.rules;
+# Restarting auditd service
+service auditd restart;
+# Please run these commands and find the adminus+ process
+# ps aux | grep pam 
+# lsof -p 8015 | grep pam_
