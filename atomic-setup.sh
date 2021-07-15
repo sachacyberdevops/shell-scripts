d99c54235f27b092beffb566a4da321652e492db
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-07-15 10:13:05 +1000

    Update atomic-setup.sh

--- atomic-setup.sh
+++ atomic-setup.sh
@@ -14,8 +14,6 @@ sudo apt-get update -y;
 sudo apt-get install -y powershell;
 # Start PowerShell
 pwsh;
-# Sleep for 3 seconds
-sleep 3;
 IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing);
 Install-AtomicRedTeam -getAtomics;
 Invoke-AtomicTest
