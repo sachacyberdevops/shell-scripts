cd998987aa50e1d25511023fa58abd6464a1454f
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-07-15 15:43:53 +1000

    Update linux-auditd.sh

--- linux-auditd.sh
+++ linux-auditd.sh
@@ -2,4 +2,5 @@
 
 sudo -i;
 sudo apt install -y auditd;
+sudo curl https://raw.githubusercontent.com/DFW1N/ansible-tower/main/credentials -o mitre_attack.rules;
 touch /etc/audit/rules.d/mitre_attack.rules;
